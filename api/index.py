3e78be14584383076862d9b8378cdf14b5c05f6d
sasrgg <sasrggs50@gmail.com>
2025-08-03 11:37:30 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,6 +4,9 @@ import os
 # إضافة مسار المشروع إلى sys.path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# تعيين ترميز UTF-8 للتعامل مع النصوص العربية
+os.environ['PYTHONIOENCODING'] = 'utf-8'
+
 from src.main import app
 
 # تصدير التطبيق للاستخدام مع Vercel
@@ -12,4 +15,3 @@ app = app
 # للاستخدام المحلي
 if __name__ == "__main__":
     app.run(debug=True)
-
